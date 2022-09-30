0597d317e739fa28b0c97289f35a7c890d64bb8d
deneb-dev <deneb288@gmail.com>
2022-09-30 14:28:27 +0800

    Add project files.

---
+++ NETSIBKM2_WebApp/Context/MyContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NETSIBKM2_WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETSIBKM2_WebApp.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> dbContext) : base(dbContext)
+        {
+
+        }
+
+
+
+
+
+
+        // Mengatur connection string (done)
+        // menambahkan model untuk diolah dan / atau migrasi (done)
+
+        /* 
+         Code first
+         */
+
+
+        public DbSet<Province> Provinces { get; set; }
+        public DbSet<Region> Regions { get; set; }
+    }
+}
+
+
+
+//< select asp -for= "RegionId" class= "form-select" asp - items = "ViewBag.RegionId" >
+
+                    //    < option ></ option >
+
+
+                    //    < option value = "Waktu Indonesia Barat" > Waktu Indonesia Barat</option>
+                    //         <option value = "Waktu Indonesia Tengah" > Waktu Indonesia Tengah</option>
+                    //<option value = "Waktu Indonesia Timur" > Waktu Indonesia Timur</option>
+
+//                </select>
---
+++ NETSIBKM2_WebApp/Controllers/ProvinceController.cs
@@ -0,0 +1,103 @@
+﻿using Jint.Runtime.Debugger;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NETSIBKM2_WebApp.Context;
+using NETSIBKM2_WebApp.Models;
+using System.Linq;
+
+namespace NETSIBKM2_WebApp.Controllers
+{
+    public class ProvinceController : Controller
+    {
+        MyContext myContext;
+
+        public ProvinceController(MyContext myContext)
+        {
+            this.myContext = myContext;
+        }
+
+        // GET ALL
+        // GET
+        public IActionResult Index()
+        {
+            var data = myContext.Provinces.Include(x => x.Region).ToList();
+            return View(data);
+
+        }
+
+        // GET BY ID
+        // GET
+        public IActionResult Details(int id)
+        {
+            //var data = myContext.Provinces.Find(id);
+            var data = myContext.Provinces.Include(x => x.Region).FirstOrDefault(x => x.Id.Equals(id));
+            return View(data);
+
+        }
+
+        // CREATE
+        // GET
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Province country)
+        {
+            if (ModelState.IsValid)
+            {
+                myContext.Provinces.Add(country);
+                var result = myContext.SaveChanges();
+                if (result > 0)
+                    return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        // Edit
+        // GET
+        public IActionResult Update()
+        {
+            return View();
+
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Province country)
+        {
+            if (ModelState.IsValid)
+            {
+                //myContext.Update(country).State = EntityState.Modified;
+                //myContext.SaveChanges();
+                //return RedirectToAction("Index");
+                myContext.Provinces.UpdateRange(country);
+                var result = myContext.SaveChanges();
+                if (result > 0)
+                    return RedirectToAction("Index");
+            }
+            return View();
+            }
+
+        // Hapus
+        // GET
+        public IActionResult Delete(Province country)
+        {
+            if (ModelState.IsValid)
+            {
+                myContext.Provinces.Remove(country);
+                var result = myContext.SaveChanges();
+                if (result > 0)
+                    return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+    }
+
+}
